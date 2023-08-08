291119bb8301d1593d586baad7e360553b1663dc
jjyh <janejyh@email.com>
2023-08-07 23:01:32 -0400

    refactored to input.js

--- play.js
+++ play.js
@@ -1,26 +1,8 @@
-const net = require("net");
+// const net = require("net");
 const { connect } = require("./client.js");
+const { setupInput } = require("./input.js");
 
 console.log("Connecting ...");
 connect();
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput); //an event listener for stdin. The listener will use a function called handleUserInput that runs when you receive input from your keyboard
-  return stdin;
-};
-
-//what happens when "data:" is received from stdin... in other words, what happens when a particular key is pressed on the keyboard input.
-const handleUserInput = function (key) {
-  // \u0003 maps to ctrl+c input
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
 
 setupInput();
