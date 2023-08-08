3edcd1493e45765544ed35dfd811a1986c0e45fb
jjyh <janejyh@email.com>
2023-08-07 21:39:41 -0400

    client modified

---
+++ play.js
@@ -0,0 +1,20 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',// IP address here,
+    port: 50541// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("data", (data) => {
+    console.log(data.toString());// code that does something when the connection is first established
+  });
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
